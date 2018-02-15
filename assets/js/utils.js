3716c71b46d2e64c55aade5554cca5366db450b2
Grant Powell <gjamespowell@gmail.com>
2018-02-14 23:45:47 -0500

    made the functions a little prettier

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,7 +1,7 @@
 export const merge = (...args) => Object.assign({}, ...args)
 
 export const uniqueFilter = (value, index, self) => self.indexOf(value) === index;
-export const arrayConcatReduce = (acc, curr) => acc.concat(curr)
+export const arrayConcat = (acc, curr) => acc.concat(curr)
 
 export const addTo = (base, amount) => base ? base + amount : amount
 export const highestOf = (...numbers) => numbers.reduce((acc, curr) => curr > acc ? curr : acc, 0);
@@ -9,15 +9,13 @@ export const highestOf = (...numbers) => numbers.reduce((acc, curr) => curr > ac
 export const sumCount = counts => Object.values(counts).reduce((acc, curr) => acc + curr, 0)
 export const netCount = (additions, subtractions) => sumCount(additions) - sumCount(subtractions)
 
-export const userNamesFromCounts = (counts) => Object.keys(counts)
+export const userNamesFromCounts = counts => Object.keys(counts)
 
 
 export const mergeCounts = (...counts) => counts.
-  map(count => userNamesFromCounts(count)).
-  reduce(arrayConcatReduce, []).
+  map(userNamesFromCounts).
+  reduce(arrayConcat, []).
   filter(uniqueFilter).
   reduce(
-    (acc, key) => merge(acc,
-       { [key]: highestOf(...counts.map(count => count[key])) }
-    ), {}
-  );
+    (acc, key) => merge(acc, { [key]: highestOf(...counts.map(count => count[key])) }
+  ), {});
