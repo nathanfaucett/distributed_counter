dac68937b37440fd5839fc1d969004087febc61f
Grant Powell <gjamespowell@gmail.com>
2018-03-20 00:15:06 -0400

    fully convert the buttons to 8 bit

--- assets/js/react/presentational_components/Button.js
+++ assets/js/react/presentational_components/Button.js
@@ -3,34 +3,34 @@ import styled from 'styled-components'
 
 import { MinusIcon, PlusIcon, Plus, Plus2, Minus } from './ButtonIcon'
 
-const width = () => document.getElementById("#normalizer").offsetWidth - 100;
-const border = () => 8;
-const height = () => 160;
 
 const Outer = styled.div`
-  border: ${() => border()}px solid ${props => props.color};
-  width: ${() => width()}px;
-  margin: 0 auto;
+  border: 8px solid ${props => props.color};
+  width: calc(100% - 120px);
+  margin: 0 auto
 `
 
 const Center = styled.div`
-  padding: 0 ${() => 3 * border()}px;
+  padding: 0 24px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
 
 const Level = styled.div`
   background-color: white;
-  border-left: ${() => border()}px solid ${props => props.color};
-  border-right: ${() => border()}px solid ${props => props.color};
+  border-left: 8px solid ${props => props.color};
+  border-right: 8px solid ${props => props.color};
   height: 100%;
-  padding: ${() => border()}px 0;
+  padding: 8px 0;
   margin-left: -16px;
 `
 
 const Leveln = styled(Level)`
-  width: ${props => props.level * 2 * border() +  width()}px
+  width: calc(16px + 100%);
 `
 
-const EightBitButton = ({children, color}) => (<Outer color={color}>
+const EightBitButton = ({children, color, onClick}) => (<Outer onClick={onClick} color={color}>
     <Leveln level={1} color={color}>
       <Leveln level={2} color={color}>
         <Leveln level={3} color={color}>
@@ -44,14 +44,30 @@ const EightBitButton = ({children, color}) => (<Outer color={color}>
   </Leveln>
 </Outer>)
 
-const MinusBox = ({children, onClick}) => <EightBitButton color={"red"} onClick={onClick}>{...children}</EightBitButton>
+const MinusBox = styled(EightBitButton)`
+  &:active {
+    background-color: ${props => props.color};
 
-const PlusBox = ({children, onClick}) => <EightBitButton color={"green"} onClick={onClick}>{...children}</EightBitButton>
+    ${Minus} {
+      background-color: white;
+    }
+  }
+`
 
-export const IncrementButton = ({incrementFunc}) => <PlusBox onClick={incrementFunc}>
+const PlusBox = styled(EightBitButton)`
+  *:active {
+    background-color: ${props => props.color};
+
+    ${Plus}, ${Plus2} {
+      background-color: white;
+    }
+}
+`
+
+export const IncrementButton = ({incrementFunc}) => <PlusBox color="green" onClick={incrementFunc}>
   <PlusIcon />
 </PlusBox>
 
-export const DecrementButton = ({decrementFunc}) => <MinusBox onClick={decrementFunc}>
+export const DecrementButton = ({decrementFunc}) => <MinusBox color="red" onClick={decrementFunc}>
   <MinusIcon />
 </MinusBox>
