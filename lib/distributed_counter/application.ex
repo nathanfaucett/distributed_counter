a61b8041bcaea3f996a9b97d28d91d50196be533
Grant Powell <gjamespowell@gmail.com>
2018-03-24 23:53:59 -0400

    removed the repo

--- lib/distributed_counter/application.ex
+++ lib/distributed_counter/application.ex
@@ -9,7 +9,7 @@ defmodule DistributedCounter.Application do
     # Define workers and child supervisors to be supervised
     children = [
       # Start the Ecto repository
-      supervisor(DistributedCounter.Repo, []),
+      # supervisor(DistributedCounter.Repo, []),
       # Start the endpoint when the application starts
       supervisor(DistributedCounterWeb.Endpoint, []),
       # Start your own worker by calling: DistributedCounter.Worker.start_link(arg1, arg2, arg3)
