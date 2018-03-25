f28e31b0110dbc302ced35e2cfb794ae49b70770
Grant Powell <gjamespowell@gmail.com>
2018-03-25 00:46:09 -0400

    updated ips

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :distributed_counter, DistributedCounterWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "52.11.76.64", port: 80],
+  url: [host: "52.37.173.99", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
